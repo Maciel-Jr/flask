099acacc024d8e8728439afee546f936a470b0d2
Maciel-jr <macieljr5587@gmail.com>
2023-06-29 10:50:10 -0400

    teste 2

--- main.py
+++ main.py
@@ -1,54 +1,11 @@
-from flask import Flask, jsonify, request
-from integrais.integralSimples import integraisSimples
-from flask_cors import CORS
+from flask import Flask, render_template
 
-
-    
 app = Flask(__name__)
-CORS(app, origins='*')
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return '<a href=""><button>Meu Botão</button></a>'
-
-@app.route('/api/integral/Quantidade=<quantidade>&ativoResposta=<ativoResposta>&AtivoLimite=<AtivoLimite>', methods=['GET'])
-def integral(quantidade, ativoResposta=False, AtivoLimite=False):
-
-    quantidade = int(quantidade)
-    ativoResposta = ativoResposta.lower() == 'true'
-    AtivoLimite = AtivoLimite.lower() == 'true'
-
-   
-    resultado = integraisSimples(quantidade, ativoResposta, AtivoLimite)
-
-    if ativoResposta == True:
-        if AtivoLimite == True:
-            mensagem = {
-                'Funcoes': resultado[0],
-                'Resultado': resultado[1],
-                'Limites Superiores': resultado[2],
-                'Limites Inferiores': resultado[3]
-            }
-        else:
-            mensagem = {
-                'Funcoes': resultado[0],
-                'Resultado': resultado[1]
-            }
-    else:
-        if AtivoLimite == True:
-         mensagem = {
-                'Funcoes': resultado[0],
-                'Limites Superiores': resultado[1],
-                'Limites Inferiores': resultado[2]
-            }
-        else:
-            mensagem = {
-                'Funcoes': resultado
-            }
-   
-
-    return jsonify(mensagem)
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
